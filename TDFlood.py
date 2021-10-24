de2923627be6ae39e89a8c1fb33490045efe8bef
Parsa <parsa@Parsas-MacBook-Air.local>
2021-10-24 18:55:58 -0400

    added an objective to check if flodding works

--- TDFlood.py
+++ TDFlood.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import time
+_old_API = False
 try:
     import graspi
 except:
@@ -13,65 +17,24 @@ except:
         exit()
 import threading
 import datetime
-import cbor #read about it. 
+import cbor
 import random
 
-#import networkx as nx 
-#TODO: since networkX might not be accessible to all nodes and its not
-#a defult python module, later convert networkx to a simple dictionary. 
-# but for now, for the simplicity and better undersanding and better 
-# visualization, we prefer to use networkX since it provides lots of utility 
-#functions for handling graph related objects. 
-
-def pr(msg):
-    print("*******************")
-    print(msg)
-    print("*******************")
-
-############################
-# STATES OF ASA
-############################
-global state
-global VISITED
-global Parent
-
-############################
-# Registering ASA and Objects
-############################
-global asa_handle
-err, handle = graspi.register_asa("TD_flood")
-
-if not err:
-    print("ASA registered OK")
-    asa_handle = handle
-else:
-    print("can't register ASA: "+ graspi.etext[err])
-
-topology = graspi.objective("topology")
-topology.neg = False
-topology.synch = True
-topology.value = 10 #sample value
-
-err = graspi.register_obj(asa_handle, topology, overlap=True)
-if not err:
-    print("object registered correctly")
-else:
-    print("cannot register the object: "+graspi.etext[err])
+####################################
+# Flood EX1 repeatedly
+####################################
 
 class flooder(threading.Thread):
     """Thread to flood EX1 repeatedly"""
     global keep_going
-    
     def __init__(self):
         threading.Thread.__init__(self, daemon=True)
 
     def run(self):
-        pr("started flooding!")
-        print("flooding "+topology.name)
-        while True:
+        while keep_going:
             time.sleep(60)
             obj1.value = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M UTC from Briggs")
-            err = graspi.flood(asa_handle, 59000, [graspi.tagged_objective(topology,None)])
+            err = graspi.flood(asa_handle, 59000, [graspi.tagged_objective(obj1,None)])
             if err:
                 graspi.tprint("Flood failure:",graspi.etext[err])
             time.sleep(5)
@@ -81,7 +44,70 @@ class flooder(threading.Thread):
             else:
                 if graspi.grasp.test_mode:
                     dump_some()
-            pr("exiting flooding")
-            graspi.tprint("Flooder exiting")
+        graspi.tprint("Flooder exiting")
+
+
+###################################
+# Print obj_registry and flood cache
+###################################
+
+def dump_some():
+    graspi.dump_all(partial=True)
+    time.sleep(5)
+
+######################
+# Main code starts
+######################                
+
+global _prng, reserves, asa_handle
+
+try:
+    graspi.checkrun
+except:
+    #not running under ASA loader
+    graspi.tprint("==========================")
+    graspi.tprint("ASA Briggs is starting up.")
+    graspi.tprint("==========================")
+    graspi.tprint("Briggs is a demonstration Autonomic Service Agent.")
+    graspi.tprint("It tests out several basic features of GRASP, and")
+    graspi.tprint("then runs indefinitely as one side of a negotiation.")
+    graspi.tprint("It acts as the banker, giving out money, and can")
+    graspi.tprint("handle multiple overlapping negotiations.")
+    graspi.tprint("The sum available is random for each negotiation,")
+    graspi.tprint("and the negotiation timeout is changed at random.")
+    graspi.tprint("On Windows or Linux, there should be a nice window")
+    graspi.tprint("that displays the negotiation process.")
+    graspi.tprint("==========================")
+
+    time.sleep(8) # so the user can read the text
+    
+_prng = random.SystemRandom() # best PRNG we can get
+keep_going = True
+
+####################################
+# Register ASA/objectives
+####################################
+
+err,asa_handle = graspi.register_asa("Briggs")
+if not err:
+    graspi.tprint("ASA Briggs registered OK")
+else:
+    graspi.tprint("Cannot register ASA:", graspi.etext[err])
+    keep_going = False
+    
+obj1 = graspi.objective("EX1")
+obj1.loop_count = 4
+obj1.synch = True
+
+err = graspi.register_obj(asa_handle,obj1)
+if not err:
+    graspi.tprint("Objective EX1 registered OK")
+else:
+    graspi.tprint("Cannot register objective:", graspi.etext[err])
+    keep_going = False
+
+dump_some()
 
 flooder().start()
+graspi.tprint("Flooding EX1 for ever")
+
