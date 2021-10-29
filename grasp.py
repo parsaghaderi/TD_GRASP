d513faa245708ef46273373f3212273879dfe915
Parsa <parsa@parsas-air.wireless.concordia.ca>
2021-10-29 19:55:10 -0400

    just checking something

--- grasp.py
+++ grasp.py
@@ -2510,7 +2510,7 @@ def flood(asa_handle, ttl, tagged_obj):
         if errorcode:
             return errorcode
 
-    if DULL or (tagged_objs[0].source and (tagged_objs[0].source.locator == _unspec_address)):
+    if DULL or (tagged_obj[0].source and (tagged_obj[0].source.locator == _unspec_address)):
         tagged_objs[0].objective.loop_count = 1 # force link-local loop count
         _local_flood = True
     else:
