0d0666a1b65ab282cb896015714e7a8436cc3b76
Parsa <parsa@parsas-air.wireless.concordia.ca>
2021-11-01 14:28:53 -0400

    fixing typo

--- refined/server.py
+++ refined/server.py
@@ -143,7 +143,7 @@ class negotiator(threading.Thread):
             step = 1
             mprint("in _{}_ step of negotiation".format(step))
             print("*************************")
-            answer.value = cbor.loads(answer)
+            answer.value = cbor.loads(answer.value)
             print(answer.value)
             print("*************************")
             self.obj.value.update(answer.value) 
