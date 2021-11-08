60fef21867c2a732ec8dac38ca06c7098e46988b
Parsa <parsa@parsas-air.wireless.concordia.ca>
2021-11-08 12:16:39 -0500

    load received objective to map2 on client side

--- refined/client.py
+++ refined/client.py
@@ -131,8 +131,8 @@ while keep_going:
 
         time.sleep(5)
     elif (not err) and nhandle:
-        nhandle = cbor.loads(nhandle)
-        mprint("requested {}, session_handle {}".format(answer, nhandle))
+        map2 = cbor.loads(nhandle)
+        mprint("requested {}, session_handle {}".format(answer, map2.value))
         if _cbor:
             answer.value = cbor.loads(answer.value)
 
