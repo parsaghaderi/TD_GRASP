600ee787add916ab0474d3f70cc47d603ad5b7c5
Parsa <parsa@Parsas-MacBook-Air.local>
2022-01-20 13:14:50 -0500

    adding multithreading to negotiation on client side

--- refined/client.py
+++ refined/client.py
@@ -143,93 +143,191 @@ else:
 failcnt =0
 _cbor = True
 mprint("start negotiation")
-while keep_going:
-    _, ll = graspi.discover(asa_handle, map2, 10000, flush=True)
+# while keep_going:
+#     _, ll = graspi.discover(asa_handle, map2, 10000, flush=True)
     
-    if ll ==[]:
-        mprint("discovery failed")
-        mprint("exiting now")
-        exit()
-    print("###############################")
-    print("discovered locator \n\t")
-    print(ll[0].locator)
-    print("###############################")
+#     if ll ==[]:
+#         mprint("discovery failed")
+#         mprint("exiting now")
+#         exit()
+#     print("###############################")
+#     print("discovered locator \n\t")
+#     print(ll[0].locator)
+#     print("###############################")
 
-    print("###############################")
-    print("list of discovered locator\n\t")
-    for x in ll:
-        print(x.locator)
-    print("###############################")
-    if _cbor:
-        map2.value=cbor.dumps(map2.value)
-        mprint("value encoded")
-    if _old_API:
-        mprint("old_api true")
-        err, nhandle, answer = graspi.req_negotiate(asa_handle, map2, ll[0], None)
-        reason = answer
-    else:
-        mprint("old_api false")#√
-        err, nhandle, answer, reason = graspi.request_negotiate(asa_handle, map2, ll[0], None)#√
+#     print("###############################")
+#     print("list of discovered locator\n\t")
+#     for x in ll:
+#         print(x.locator)
+#     print("###############################")
+#     if _cbor:
+#         map2.value=cbor.dumps(map2.value)
+#         mprint("value encoded")
+#     if _old_API:
+#         mprint("old_api true")
+#         err, nhandle, answer = graspi.req_negotiate(asa_handle, map2, ll[0], None)
+#         reason = answer
+#     else:
+#         mprint("old_api false")#√
+#         err, nhandle, answer, reason = graspi.request_negotiate(asa_handle, map2, ll[0], None)#√
     
-    if err:
-        if err==graspi.errors.declined and reason!="":
-            _e = reason
-        else:
-            _e = graspi.etext[err]
-        mprint("request_negotiate error: "+ _e)
-        failcnt+=1
-        print("########################")
-        print("fail count:\n\t")
-        print(failcnt)
-        print("########################")
+#     if err:
+#         if err==graspi.errors.declined and reason!="":
+#             _e = reason
+#         else:
+#             _e = graspi.etext[err]
+#         mprint("request_negotiate error: "+ _e)
+#         failcnt+=1
+#         print("########################")
+#         print("fail count:\n\t")
+#         print(failcnt)
+#         print("########################")
 
-        time.sleep(5)
-    elif (not err) and nhandle:
+#         time.sleep(5)
+#     elif (not err) and nhandle:
         
-        mprint("requested {}".format(answer.name))#√
-        if _cbor:
-            mprint(answer.value)#√
-        map2.value = cbor.loads(map2.value)#√
-        mprint("peer offered {}".format(answer.value))#√
-        map2.value.update(answer.value)#√
+#         mprint("requested {}".format(answer.name))#√
+#         if _cbor:
+#             mprint(answer.value)#√
+#         map2.value = cbor.loads(map2.value)#√
+#         mprint("peer offered {}".format(answer.value))#√
+#         map2.value.update(answer.value)#√
 
-        mprint("updated value for map2 {}".format(map2.value))#√
-        #answer.value = cbor.dumps(map2.value) #TODO
-        _r = graspi.negotiate_step(asa_handle, nhandle, answer, 1000)
-        if _old_API:
-            err, temp, answer = _r
-            reason = answer
-        else:
-            err, temp, answer, reason = _r
-        if _cbor and (not err):
-            try:
-                answer.value = cbor.loads(answer.value)
-            except:
-                pass
-        if (not err) and temp == None:
-            mprint("negotiation succeeded")
-            mprint("updated value of answer: "+answer.value)
-            map2.value = answer.value
-        else:
-            mprint("negotiation error "+graspi.etext[err])
-            mprint("exiting now")
-            exit()
-        err = graspi.end_negotiate(asa_handle, nhandle, True)
-        if not err:
-            print("##############################################")
-            print("negotiation ended successfully with answer\n\t")
-            print(map2.value)
-            print("##############################################\n")
-        else:
-            mprint("end negotiation error: "+graspi.etext[err])
+#         mprint("updated value for map2 {}".format(map2.value))#√
+#         #answer.value = cbor.dumps(map2.value) #TODO
+#         _r = graspi.negotiate_step(asa_handle, nhandle, answer, 1000)
+#         if _old_API:
+#             err, temp, answer = _r
+#             reason = answer
+#         else:
+#             err, temp, answer, reason = _r
+#         if _cbor and (not err):
+#             try:
+#                 answer.value = cbor.loads(answer.value)
+#             except:
+#                 pass
+#         if (not err) and temp == None:
+#             mprint("negotiation succeeded")
+#             mprint("updated value of answer: "+answer.value)
+#             map2.value = answer.value
+#         else:
+#             mprint("negotiation error "+graspi.etext[err])
+#             mprint("exiting now")
+#             exit()
+#         err = graspi.end_negotiate(asa_handle, nhandle, True)
+#         if not err:
+#             print("##############################################")
+#             print("negotiation ended successfully with answer\n\t")
+#             print(map2.value)
+#             print("##############################################\n")
+#         else:
+#             mprint("end negotiation error: "+graspi.etext[err])
         
-    else:
-        err = graspi.end_negotiate(asa_handle, nhandle, True)
-        if not err:
-            print("##############################################")
-            print("negotiation ended successfully with answer\n\t")
-            print(map2.value)
-            print("##############################################\n")
-        else:
-            mprint("end negotiation error: "+graspi.etext[err])
+#     else:
+#         err = graspi.end_negotiate(asa_handle, nhandle, True)
+#         if not err:
+#             print("##############################################")
+#             print("negotiation ended successfully with answer\n\t")
+#             print(map2.value)
+#             print("##############################################\n")
+#         else:
+#             mprint("end negotiation error: "+graspi.etext[err])
 
+
+
+class negotiator(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    
+    def run(self):
+        while keep_going:
+            _, ll = graspi.discover(asa_handle, map2, 10000, flush=True)
+            
+            if ll ==[]:
+                mprint("discovery failed")
+                mprint("exiting now")
+                exit()
+            print("###############################")
+            print("discovered locator \n\t")
+            print(ll[0].locator)
+            print("###############################")
+
+            print("###############################")
+            print("list of discovered locator\n\t")
+            for x in ll:
+                print(x.locator)
+            print("###############################")
+            if _cbor:
+                map2.value=cbor.dumps(map2.value)
+                mprint("value encoded")
+            if _old_API:
+                mprint("old_api true")
+                err, nhandle, answer = graspi.req_negotiate(asa_handle, map2, ll[0], None)
+                reason = answer
+            else:
+                mprint("old_api false")#√
+                err, nhandle, answer, reason = graspi.request_negotiate(asa_handle, map2, ll[0], None)#√
+            
+            if err:
+                if err==graspi.errors.declined and reason!="":
+                    _e = reason
+                else:
+                    _e = graspi.etext[err]
+                mprint("request_negotiate error: "+ _e)
+                failcnt+=1
+                print("########################")
+                print("fail count:\n\t")
+                print(failcnt)
+                print("########################")
+
+                time.sleep(5)
+            elif (not err) and nhandle:
+                
+                mprint("requested {}".format(answer.name))#√
+                if _cbor:
+                    mprint(answer.value)#√
+                map2.value = cbor.loads(map2.value)#√
+                mprint("peer offered {}".format(answer.value))#√
+                map2.value.update(answer.value)#√
+
+                mprint("updated value for map2 {}".format(map2.value))#√
+                #answer.value = cbor.dumps(map2.value) #TODO
+                _r = graspi.negotiate_step(asa_handle, nhandle, answer, 1000)
+                if _old_API:
+                    err, temp, answer = _r
+                    reason = answer
+                else:
+                    err, temp, answer, reason = _r
+                if _cbor and (not err):
+                    try:
+                        answer.value = cbor.loads(answer.value)
+                    except:
+                        pass
+                if (not err) and temp == None:
+                    mprint("negotiation succeeded")
+                    mprint("updated value of answer: "+answer.value)
+                    map2.value = answer.value
+                else:
+                    mprint("negotiation error "+graspi.etext[err])
+                    mprint("exiting now")
+                    exit()
+                err = graspi.end_negotiate(asa_handle, nhandle, True)
+                if not err:
+                    print("##############################################")
+                    print("negotiation ended successfully with answer\n\t")
+                    print(map2.value)
+                    print("##############################################\n")
+                else:
+                    mprint("end negotiation error: "+graspi.etext[err])
+                
+            else:
+                err = graspi.end_negotiate(asa_handle, nhandle, True)
+                if not err:
+                    print("##############################################")
+                    print("negotiation ended successfully with answer\n\t")
+                    print(map2.value)
+                    print("##############################################\n")
+                else:
+                    mprint("end negotiation error: "+graspi.etext[err])
+
+negotiator().start()
